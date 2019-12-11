816a420f1ccaf2d0896cbf9b645fcffb0d351e5a
James Douglass <jdouglass@stanford.edu>
2019-12-11 15:17:36 -0800

    Updating links to landsat band information.

--- ndvi.py
+++ ndvi.py
@@ -2,7 +2,8 @@
 """Calculate NDVI from a Landsat 8 scene."""
 
 # For more information about landsat 8 bands, see:
-# https://landsat.usgs.gov/what-are-band-designations-landsat-satellites
+# https://www.usgs.gov/media/images/landsat-8-band-designations
+# https://www.usgs.gov/faqs/how-does-data-sentinel-2a-s-multispectral-instrument-compare-landsat-data
 RED_2013 = "data/120m/LC08_L1TP_042034_20130605_20170310_01_T1_B4_120x120.TIF"
 NIR_2013 = "data/120m/LC08_L1TP_042034_20130605_20170310_01_T1_B5_120x120.TIF"
 RED_2016 = "data/120m/LC08_L1TP_042034_20160629_20170222_01_T1_B4_120x120.TIF"
